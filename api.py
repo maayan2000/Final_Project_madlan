0c7f92736fdcd5c8049f2551778eb8353598a33b
maayan2000 <88968205+maayan2000@users.noreply.github.com>
2023-06-26 22:55:40 +0300

    Add files via upload

--- api.py
+++ api.py
@@ -1,46 +1,8 @@
-import numpy as np
-from flask import Flask, request, jsonify, render_template
-import pickle
 import os
+import pickle
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
-rf_model = pickle.load(open('trained_model.pkl', 'rb'))
-
-# City mapping dictionary
-city_mapping = {
-    'נהריה': 0.25806451612903225,
-    'שוהם': 0.8387096774193549,
-    'אילת': 0.1935483870967742,
-    'אריאל': 0.22580645161290322,
-    'באר שבע': 0.06451612903225806,
-    'בית שאן': 0.0967741935483871,
-    'בת ים': 0.5483870967741935,
-    'גבעת שמואל': 0.6774193548387096,
-    'דימונה': 0.0,
-    'הוד השרון': 0.9354838709677419,
-    'הרצליה': 0.7419354838709677,
-    'זכרון יעקב': 0.7096774193548387,
-    'חולון': 0.41935483870967744,
-    'חיפה': 0.2903225806451613,
-    'יהוד מונוסון': 0.6129032258064516,
-    'ירושלים': 1.0,
-    'כפר סבא': 0.5806451612903226,
-    'מודיעין מכבים רעות': 0.6451612903225806,
-    'נהריה': 0.16129032258064516,
-    'נהרייה': 0.45161290322580644,
-    'נוף הגליל': 0.03225806451612903,
-    'נס ציונה': 0.7741935483870968,
-    'נתניה': 0.5161290322580645,
-    'פתח תקווה': 0.4838709677419355,
-    'צפת': 0.3548387096774194,
-    'קרית ביאליק': 0.12903225806451613,
-    'ראשון לציון': 0.3870967741935484,
-    'רחובות': 0.3225806451612903,
-    'רמת גן': 0.8709677419354839,
-    'רעננה': 0.8064516129032258,
-    'שוהם': 0.967741935483871,
-    'תל אביב': 0.9032258064516129
-}
 
 @app.route('/')
 def home():
@@ -48,30 +10,35 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
-    features = [
-        'City', 'room_number', 'Area', 'city_area', 'hasElevator', 'hasParking',
-        'hasBars', 'hasStorage', 'hasAirCondition', 'hasBalcony', 'hasMamad',
-        'handicapFriendly', 'floor', 'total_floors', 'current_entrance_date'
-    ]
-    
+    features = ['room_number', 'Area', 'hasElevator', 'hasParking', 'hasBars',
+                'hasStorage', 'hasAirCondition', 'hasBalcony', 'hasMamad',
+                'handicapFriendly', 'floor', 'total_floors',
+                'City_נהריה', 'City_שוהם', 'City_אילת', 'City_אריאל', 'City_באר שבע',
+                'City_בית שאן', 'City_בת ים', 'City_גבעת שמואל', 'City_דימונה',
+                'City_הוד השרון', 'City_הרצליה', 'City_זכרון יעקב', 'City_חולון',
+                'City_חיפה', 'City_יהוד מונוסון', 'City_ירושלים', 'City_כפר סבא',
+                'City_מודיעין מכבים רעות', 'City_נהריה', 'City_נהרייה',
+                'City_נוף הגליל', 'City_נס ציונה', 'City_נתניה', 'City_פתח תקווה',
+                'City_צפת', 'City_קרית ביאליק', 'City_ראשון לציון', 'City_רחובות',
+                'City_רמת גן', 'City_רעננה', 'City_שוהם', 'City_תל אביב']
     feature_values = [request.form.get(feature) for feature in features]
-    
-    # Map the City feature to its corresponding numerical value
-    city_value = city_mapping.get(feature_values[0])
-    feature_values[0] = city_value if city_value is not None else 0.0
-    
-    #final_features = np.array(feature_values).reshape(1, -1)
-    # Ensure that all values in final_features are valid and convertable to float
-    final_features = [float(value) if value.isdigit() else 0.0 for value in features]
+
+    # Load the model
+    model_path = 'trained_model.pkl'
+    if not os.path.isfile(model_path):
+        return render_template('index.html', prediction_text='Model file not found.')
+
+    with open(model_path, 'rb') as file:
+        model = pickle.load(file)
+
+    # Convert the feature values to a 2D array
+    feature_array = [list(map(int, feature_values))]
 
     # Make the prediction
-    prediction = rf_model.predict([final_features])[0]
-    
-    output_text = f"The predicted price is: {prediction}"
-    
+    prediction = model.predict(feature_array)[0]
+
+    output_text = f"The predicted value is: {prediction}"
+
     return render_template('index.html', prediction_text=output_text)
 
 
